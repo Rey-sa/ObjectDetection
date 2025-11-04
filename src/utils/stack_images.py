afecc65c82c3cc3b6bb56df877824001b06007fd
rey <saskia.rey96@gmail.com>
2025-11-04 12:45:46 +0100

    Added labels to video streams

--- src/utils/stack_images.py
+++ src/utils/stack_images.py
@@ -1,13 +1,19 @@
 import cv2 as cv
 import numpy as np
 
-def stack_images(scale, vid_array):
+def stack_images(scale, vid_array, labels=None):
     rows = len(vid_array)
     cols = len(vid_array[0]) if isinstance(vid_array[0], list) else len(vid_array)
     rows_available = isinstance(vid_array[0], list)
 
     width = vid_array[0][0].shape[1] if rows_available else vid_array[0].shape[1]
     height = vid_array[0][0].shape[0] if rows_available else vid_array[0].shape[0]
+
+    if labels is None:
+        if rows_available:
+            labels = [[""] * cols  for _ in range(rows)]
+        else:
+            labels = [""] * cols
 
     if rows_available:
         for x in range(rows):
@@ -21,6 +27,12 @@ def stack_images(scale, vid_array):
                 # Convert grayscale to BGR
                 if len(vid_array[x][y].shape) == 2:
                     vid_array[x][y] = cv.cvtColor(vid_array[x][y], cv.COLOR_GRAY2BGR)
+
+                # Add Label
+                if labels[x][y]:
+                    cv.putText(vid, labels[x][y], (10, 30),cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0),2)
+
+                vid_array[x][y] = vid
 
             # Horizontal / vertical chaining
             hor = [np.hstack(vid_array[x]) for x in range(rows)]
